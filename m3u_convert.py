399a523ff1c96e95e801dc0e8f9a73d9712b7e55
Winges4k <87330083+Winges4k@users.noreply.github.com>
2021-09-05 18:37:24 +0200

    Update m3u_convert.py

--- m3u_convert.py
+++ m3u_convert.py
@@ -14,7 +14,7 @@ f.write('#EXTM3U\n')
 for i in range(len(channels['Categories'])):
     for j in range(len(channels['Categories'][i]['Channels'])):
         line_1 = '#EXTINF:-1 group-title="' + channels['Categories'][i]['Name'] + '",' + channels['Categories'][i]['Channels'][j]['Name'] + '\n'
-        m3u8_url = 'https://iptv.tsinghua.edu.cn/hls/' + channels['Categories'][i]['Channels'][j]['Vid'] + '.m3u8'
+        m3u8_url = 'https://github.com/bitsbb01/ez-iptvcat-scraper2/blob/main/data/countries/' + channels['Categories'][i]['Channels'][j]['Vid'] + '.m3u8'
         f.write(line_1)
         f.write(m3u8_url + '\n')
 
