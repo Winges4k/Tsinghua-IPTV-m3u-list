3d96d9edc648a897509f3d80c7f67d28980e3e05
Winges4k <87330083+Winges4k@users.noreply.github.com>
2021-12-20 21:17:11 +0100

    Update m3u_convert.py

--- m3u_convert.py
+++ m3u_convert.py
@@ -3,7 +3,7 @@
 from urllib import request
 import json
 
-channels_url = 'https://github.com/bitsbb01/ez-iptvcat-scraper2/blob/main/data/countries/france.json'
+channels_url = 'https://github.com/ericziethen/ez-iptvcat-scraper/blob/master/data/countries/france.json'
 channels_json = request.urlopen(channels_url).read().decode('utf8')
 channels = json.loads(channels_json)
 
@@ -14,7 +14,7 @@ f.write('#EXTM3U\n')
 for i in range(len(channels['Categories'])):
     for j in range(len(channels['Categories'][i]['Channels'])):
         line_1 = '#EXTINF:-1 group-title="' + channels['Categories'][i]['Name'] + '",' + channels['Categories'][i]['Channels'][j]['Name'] + '\n'
-        m3u8_url = 'https://github.com/bitsbb01/ez-iptvcat-scraper2/blob/main/data/countries/' + channels['Categories'][i]['Channels'][j]['Vid'] + '.m3u8'
+        m3u8_url = 'https://github.com/ericziethen/ez-iptvcat-scraper/blob/master/data/countries/' + channels['Categories'][i]['Channels'][j]['Vid'] + '.m3u8'
         f.write(line_1)
         f.write(m3u8_url + '\n')
 
